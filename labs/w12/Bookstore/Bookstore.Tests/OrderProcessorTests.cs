357ade24f490ad2574dbacf7726f6d4a2c8cb236
gFalkSyss8 <gustav.arnefalk@edu.newton.se>
2026-03-19 14:35:07 +0100

    Created further tests for remaining methods

---
+++ labs/w12/Bookstore/Bookstore.Tests/OrderProcessorTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bookstore;
+
+namespace Bookstore.Tests;
+
+[TestClass]
+public class OrderProcessorTests
+{
+    private OrderProcessor _processor;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _processor = new OrderProcessor();
+    }
+
+    [TestMethod]
+    public void TotalCostCalculator_EnsureValidValue()
+    {
+        // Arrange
+        int numberOfItems = 10;
+        decimal unitPrice = 250M;
+
+        // Act 
+        var result = _processor.CalculateTotalCost(numberOfItems, unitPrice);
+
+        // Assert
+        Assert.IsInstanceOfType(result, typeof(decimal));
+    }
+}
---
+++ labs/w12/Bookstore/Bookstore.Tests/UserRegistrationTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bookstore;
+
+namespace Bookstore.Tests;
+
+[TestClass]
+public class UserRegistrationTests
+{
+    private UserRegistration _register;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _register = new UserRegistration();
+    }
+
+    [TestMethod]
+    public void RegisterNewUser_ShouldReturnTrue()
+    {
+        // Arrange
+        string username = "Gustav";
+
+        // Act 
+        var result = _register.RegisterUser(username);
+
+        // Assert
+        Assert.IsTrue(result);
+    }
+}

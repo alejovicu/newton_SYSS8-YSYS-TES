b95deb5051d554f8521b6afe5f78f2d5e0591a7e
Axel-Newton <axel.larsson@edu.newton.se>
2026-03-20 14:28:42 +0100

    In BookstoreInventory.cs: removebook method  to decrease stock instead of increase. In BookstoreInventoryTests.cs: Added 3 unit tests.  TestAddBook, TestRemoveBook and TestCheckStockNotExists

--- labs/w12/Bookstore/Bookstore.Tests/BookstoreInventoryTests.cs
+++ labs/w12/Bookstore/Bookstore.Tests/BookstoreInventoryTests.cs
@@ -15,10 +15,48 @@ public class BookstoreInventoryTests
     }
 
     [TestMethod]
-    public void Test1()
+    public void TestAddBook()
     {
-        //Implement tests
-        Assert.IsTrue(true);
+        // arrange 
+        var testbook = new Book("001", "Percy Jackson", "Rick Riordan", 12);
+        
+        // act
+        _inventory.AddBook(testbook);
+        
+        // assert
+        var stock = _inventory.CheckStock("001");
+        Assert.AreEqual(12, stock);
+    }
+
+    [TestMethod]
+    public void TestRemoveBook()
+    {
+        // arrange 
+        var onepiece = new Book("002", "One Piece", "Eiichiro Oda", 7); // add book to remove
+        _inventory.AddBook(onepiece);
+        
+        var booktoremove = _inventory.FindBookByTitle("One Piece");
+        Assert.IsNotNull(booktoremove);
+        var newStock = 6;
+        
+        // act
+        _inventory.RemoveBook(booktoremove.ISBN); //remove book
+       
+        // assert
+        Assert.AreEqual(newStock, booktoremove.Stock); //check that book is removed
+    }
+
+    [TestMethod]
+    public void TestCheckStockNotExists()
+    {
+        // arrange 
+        // no book created
+        
+        // act
+        
+        // assert
+        var stock = _inventory.CheckStock("000");
+        Assert.AreEqual(0, stock);
     }
 
 }

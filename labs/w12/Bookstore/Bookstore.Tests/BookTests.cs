0fae236990832dfc21c638ab8e48f4febc0cf909
Merhawit Mebrahtu <merhawitristet431@gmail.com>
2026-03-21 18:49:14 +0100

    add BookTest and add AddBookNewBookReturnsTrue, AddBookExistingBookIncreasesStock and RemoveBookBookExistsReturnsTrue in  BookstoreInventoryTests

---
+++ labs/w12/Bookstore/Bookstore.Tests/BookTests.cs
@@ -0,0 +1,30 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bookstore;
+
+namespace Bookstore.Tests;
+
+[TestClass]
+public class BookTests
+{
+    
+    [TestMethod]
+    public void TestBookConstructorSetsPropertiesCorrectly()
+    {
+        // Arrange
+        string isbn = "123";
+        string title = "Clean Code";
+        string author = "Robert Martin";
+        int stock = 5;
+
+        // Act
+        var book = new Book(isbn, title, author, stock);
+
+        // Assert
+        Assert.AreEqual("123", book.ISBN);
+        Assert.AreEqual("Clean Code", book.Title);
+        Assert.AreEqual("Robert Martin", book.Author);
+        Assert.AreEqual(5, book.Stock);
+    }
+    
+}
--- labs/w12/Bookstore/Bookstore.Tests/BookstoreInventoryTests.cs
+++ labs/w12/Bookstore/Bookstore.Tests/BookstoreInventoryTests.cs
@@ -1,7 +1,8 @@
+
+namespace Bookstore.Tests;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Bookstore;
 
-namespace Bookstore.Tests;
 
 [TestClass]
 public class BookstoreInventoryTests
@@ -20,5 +21,51 @@ public class BookstoreInventoryTests
         //Implement tests
         Assert.IsTrue(true);
     }
+
+ [TestMethod]
+    public void AddBookNewBookReturnsTrue()
+    {
+        // Arrange
+        var book = new Book("123", "Clean Code", "Robert Martin", 5);
+
+        // Act
+        bool result = _inventory.AddBook(book);
+
+        // Assert
+        Assert.IsTrue(result);
+        Assert.AreEqual(5, _inventory.CheckStock("123"));
+    }
+
+ [TestMethod]
+    public void AddBookExistingBookIncreasesStock()
+    {
+        // Arrange
+        var book1 = new Book("123", "Clean Code", "Robert Martin", 5);
+        var book2 = new Book("123", "Clean Code", "Robert Martin", 3);
+
+        _inventory.AddBook(book1);
+
+        // Act
+        bool result = _inventory.AddBook(book2);
+
+        // Assert
+        Assert.IsFalse(result);
+        Assert.AreEqual(8, _inventory.CheckStock("123"));
+    }
+
+[TestMethod]
+    public void RemoveBookBookExistsReturnsTrue()
+    {
+        // Arrange
+        var book = new Book("111", "C# Basics", "Alice", 3);
+        _inventory.AddBook(book);
+
+        // Act
+        bool result = _inventory.RemoveBook("111");
+
+        // Assert
+        Assert.IsTrue(result);
+    }
+
 
 }

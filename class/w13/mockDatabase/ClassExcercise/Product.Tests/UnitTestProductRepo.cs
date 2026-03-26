d0de80d25f8984945a7877b6c3bd08a67dd58688
gFalkSyss8 <gustav.arnefalk@edu.newton.se>
2026-03-26 10:37:15 +0100

    Created new MSTest project for unit and integration test assignment

---
+++ class/w13/mockDatabase/ClassExcercise/Product.Tests/UnitTestProductRepo.cs
@@ -0,0 +1,62 @@
+﻿namespace ProductManager.Tests
+{
+
+    [TestClass]
+    public sealed class UnitTestProductRepo
+    {
+
+        [TestMethod]
+        [TestCategory("Integration")]
+        public void TestGetProductByCategory_ShouldBeListOfProduct()
+        {
+            // Arrange
+            var productRepository = new ProductRepository();
+            var expectedCategoryTerm = "Technology";
+
+            // Act
+            var result = productRepository.GetProductsByCategory(expectedCategoryTerm);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(List<Product>));
+        }
+
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void TestGetProductByCategory_ValidateExpected()
+        {
+            // Arrange
+            var productRepository = new ProductRepository();
+            var expectedProductCount = 1;
+            var expectedCategoryTerm = "Technology";
+
+            // Act
+            var result = productRepository.GetProductsByCategory(expectedCategoryTerm);
+
+            // Assert
+            Assert.HasCount(expectedProductCount, result);
+            Assert.AreEqual(expectedCategoryTerm, result[0].Category);
+        }
+
+        [TestMethod]
+        [TestCategory("Unit")]
+        [TestCategory("Slow")]
+        public void TestGetProductByCategory_EnsureAllReturnTypesMatchAndValidateName()
+        {
+            // Arrange
+            var productRepository = new ProductRepository();
+            var expectedCategoryTerm = "Technology";
+
+            // Act
+            var result = productRepository.GetProductsByCategory(expectedCategoryTerm);
+
+            // Assert
+            foreach (var product in result)
+            {
+                Assert.AreEqual(expectedCategoryTerm, product.Category);
+                Assert.IsNotEmpty(product.Name);
+            }
+
+        }
+
+    }
+}

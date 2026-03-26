a5868a2979ca969ac5351b1f16c1eddceded026c
gFalkSyss8 <gustav.arnefalk@edu.newton.se>
2026-03-26 12:39:45 +0100

    Updated unit testing to use mock data

--- class/w13/mockDatabase/ClassExcercise/Product.Tests/UnitTestProductRepo.cs
+++ class/w13/mockDatabase/ClassExcercise/Product.Tests/UnitTestProductRepo.cs
@@ -1,4 +1,8 @@
-﻿namespace ProductManager.Tests
+﻿using Npgsql;
+using System.Data;
+using Moq;
+
+namespace ProductManager.Tests
 {
 
     [TestClass]
@@ -25,9 +29,28 @@
         public void TestGetProductByCategory_ValidateExpected()
         {
             // Arrange
-            var productRepository = new ProductRepository();
             var expectedProductCount = 1;
             var expectedCategoryTerm = "Technology";
+
+            var mockConnection = new Mock<IDbConnection>();
+            var mockCommand = new Mock<IDbCommand>();
+            var mockReader = new Mock<IDataReader>();
+
+            // Setup mock Read to increment and read value
+            var readCallCount = 0;
+            mockReader.Setup(r => r.Read()).Returns(() => readCallCount++ == 0);
+
+            // Mock return values for ID, Name and Category
+            mockReader.Setup(r => r.GetInt32(0)).Returns(1);
+            mockReader.Setup(r => r.GetString(1)).Returns("iPhone 17 Pro");
+            mockReader.Setup(r => r.GetString(2)).Returns("Technology");
+
+            // Setup ExecuteReader() to return our mock reader
+            mockCommand.Setup(c => c.ExecuteReader()).Returns(mockReader.Object);
+            // Setup CreateCommand() to return our mock command
+            mockConnection.Setup(c => c.CreateCommand()).Returns(mockCommand.Object);
+
+            var productRepository = new ProductRepository(mockConnection.Object);
 
             // Act
             var result = productRepository.GetProductsByCategory(expectedCategoryTerm);
@@ -43,8 +66,27 @@
         public void TestGetProductByCategory_EnsureAllReturnTypesMatchAndValidateName()
         {
             // Arrange
-            var productRepository = new ProductRepository();
             var expectedCategoryTerm = "Technology";
+
+            var mockConnection = new Mock<IDbConnection>();
+            var mockCommand = new Mock<IDbCommand>();
+            var mockReader = new Mock<IDataReader>();
+
+            // Setup mock Read to increment and read value
+            var readCallCount = 0;
+            mockReader.Setup(r => r.Read()).Returns(() => readCallCount++ == 0);
+
+            // Mock return values for ID, Name and Category
+            mockReader.Setup(r => r.GetInt32(0)).Returns(1);
+            mockReader.Setup(r => r.GetString(1)).Returns("iPhone 17 Pro");
+            mockReader.Setup(r => r.GetString(2)).Returns("Technology");
+
+            // Setup ExecuteReader() to return our mock reader
+            mockCommand.Setup(c => c.ExecuteReader()).Returns(mockReader.Object);
+            // Setup CreateCommand() to return our mock command
+            mockConnection.Setup(c => c.CreateCommand()).Returns(mockCommand.Object);
+
+            var productRepository = new ProductRepository(mockConnection.Object);
 
             // Act
             var result = productRepository.GetProductsByCategory(expectedCategoryTerm);
--- class/w13/mockDatabase/ClassExcercise/Product/ProductRepository.cs
+++ class/w13/mockDatabase/ClassExcercise/Product/ProductRepository.cs
@@ -3,27 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 using Npgsql;
 
 namespace ProductManager
 {
     public class ProductRepository
     {
-        private readonly string _connectionString;
+        private readonly IDbConnection _connection;
 
+        // Real DB
         public ProductRepository()
         {
-            _connectionString = "Host=localhost;Port=5432;Username=postgres;Password=mysecretpassword;Database=postgres";
+            _connection = new NpgsqlConnection("Host=localhost;Port=5432;Username=postgres;Password=mysecretpassword;Database=postgres");
+        }
+
+        // Setup repo with mock when value is used
+        public ProductRepository(IDbConnection connection)
+        {
+            _connection = connection;
         }
 
         public List<Product> GetProductsByCategory(string Category)
         {
             var products = new List<Product>();
 
-            using var connection = new NpgsqlConnection(_connectionString);
-            connection.Open();
+            _connection.Open();
 
-            using var cmd = new NpgsqlCommand($"SELECT id, name, category FROM Products WHERE Category = '{Category}'", connection);
+            using var cmd = _connection.CreateCommand();
+            cmd.CommandText = $"SELECT id, name, category FROM Products WHERE Category = '{Category}'";
             using var reader = cmd.ExecuteReader();
 
             while (reader.Read())
@@ -36,7 +44,20 @@ namespace ProductManager
                 });
             }
 
+            _connection.Close();
+
             return products;
+            
+        }
+
+        public void InsertProduct(Product product)
+        {
+            _connection.Open();
+            using var cmd = _connection.CreateCommand();
+            cmd.CommandText = "INSERT INTO products (name, id, category, price) VALUES ('" + product.Name + product.Id + product.Category + product.Price + "')";
+            using var reader = cmd.ExecuteReader();
+
+            _connection.Close();
         }
     }
 }

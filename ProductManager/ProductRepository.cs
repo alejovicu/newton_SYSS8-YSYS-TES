4c8758300930aeab8cc1664a46eec56d1f06b039
themyzteziz <markus.siteus@hotmail.com>
2026-03-27 10:29:24 +0100

    Mocking

---
+++ ProductManager/ProductRepository.cs
@@ -0,0 +1,54 @@
+﻿using System.Data;
+using Npgsql;
+
+namespace ProductManager;
+
+public class ProductRepository
+{
+    private readonly IDbConnection _connection;
+
+    
+    public ProductRepository()
+    {
+        _connection = new NpgsqlConnection(
+            "Host=localhost;Port=5432;Username=postgres;Password=postgres;Database=productsdb");
+    }
+
+    
+    public ProductRepository(IDbConnection connection)
+    {
+        _connection = connection;
+    }
+
+    public List<Product> GetProductsByCategory(string category)
+    {
+        var products = new List<Product>();
+
+        _connection.Open();
+
+        using var cmd = _connection.CreateCommand();
+        cmd.CommandText = "SELECT name, category, price FROM products";
+
+        using var reader = cmd.ExecuteReader();
+
+        while (reader.Read())
+        {
+            var product = new Product
+            {
+                Name = reader.GetString(0),
+                Category = reader.GetString(1),
+                Price = reader.GetString(2)
+            };
+
+            
+            if (product.Category == category)
+            {
+                products.Add(product);
+            }
+        }
+
+        _connection.Close();
+
+        return products;
+    }
+}
--- ProductManager/ProductService.cs
+++ ProductManager/ProductService.cs
@@ -1,38 +1,16 @@
-﻿using Npgsql;
-
-namespace ProductManager;
+﻿namespace ProductManager;
 
 public class ProductService
 {
+    private readonly ProductRepository _repo;
+
+    public ProductService(ProductRepository repo)
+    {
+        _repo = repo;
+    }
+
     public List<Product> GetProductsByCategory(string category)
     {
-
-        var allProducts = new List<Product>();
-
-        using var connection = new NpgsqlConnection(
-            "Host=localhost;Port=5432;Username=postgres;Password=postgres;Database=productsdb");
-
-        connection.Open();
-
-        var command = new NpgsqlCommand(
-            "SELECT name, category, price FROM products",
-            connection);
-
-        command.Parameters.AddWithValue("@category", category);
-
-        using var reader = command.ExecuteReader();
-
-        while (reader.Read())
-        {
-            allProducts.Add(new Product
-            {
-                Name = reader.GetString(0),
-                Category = reader.GetString(1),
-                Price = reader.GetString(2)
-            });
-        }
-        connection.Close();
-
-        return result;
+        return _repo.GetProductsByCategory(category);
     }
 }

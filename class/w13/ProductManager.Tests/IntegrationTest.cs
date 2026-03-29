fd8fbdc99052c617836186b43b2180ee81e7f3d8
soheyl228 <soheylizadkhah@gmail.com>
2026-03-29 19:03:31 +0200

    Implemented ProductManager with integration and unit tests using PostgreSQL and Moq

---
+++ class/w13/ProductManager.Tests/IntegrationTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProductManager;
+using System.Linq;
+
+namespace ProductManager.Tests
+{
+    [TestClass]
+    public class ProductIntegrationTests
+    {
+        [TestMethod]
+        [TestCategory("Integration")]
+        public void GetProductsByCategory_FromDatabase()
+        {
+            // Arrange
+            var repository = new ProductRepository();
+            var service = new ProductService(repository);
+
+            // Act
+            var techProducts = service.GetProductsByCategory("Tech");
+            var foodProducts = service.GetProductsByCategory("Food");
+
+            // Assert
+            Assert.IsTrue(techProducts.Any());
+            Assert.IsTrue(foodProducts.Any());
+
+            Assert.AreEqual("Tech", techProducts[0].Category);
+            Assert.AreEqual("Food", foodProducts[0].Category);
+        }
+    }
+}
---
+++ class/w13/ProductManager.Tests/UnitTest.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ProductManager;
+using System.Collections.Generic;
+using System.Data;
+
+namespace ProductManager.Tests
+{
+    [TestClass]
+    public class ProductUnitTests
+    {
+        [TestMethod]
+        [TestCategory("UnitTest")]
+        public void GetProductsByCategory_UnitTest_WithMock()
+        {
+            // Arrange
+            var expectedCategory = "Food";
+
+            var mockConnection = new Mock<IDbConnection>();
+            var mockCommand = new Mock<IDbCommand>();
+            var mockReader = new Mock<IDataReader>();
+            var mockParameter = new Mock<IDbDataParameter>();
+            var mockParameters = new Mock<IDataParameterCollection>();
+
+            // Connection
+            mockConnection.Setup(c => c.Open());
+            mockConnection.Setup(c => c.Close());
+            mockConnection.Setup(c => c.CreateCommand()).Returns(mockCommand.Object);
+
+            // Command
+            mockCommand.Setup(c => c.ExecuteReader()).Returns(mockReader.Object);
+            mockCommand.Setup(c => c.CreateParameter()).Returns(mockParameter.Object);
+            mockCommand.Setup(c => c.Parameters).Returns(mockParameters.Object);
+
+            // Reader (simulate DB rows)
+            mockReader.SetupSequence(r => r.Read())
+                .Returns(true)
+                .Returns(true)
+                .Returns(false);
+
+            // name
+            mockReader.SetupSequence(r => r.GetString(0))
+                .Returns("Pizza")
+                .Returns("iPhone 17 Pro");
+
+            // category
+            mockReader.SetupSequence(r => r.GetString(1))
+                .Returns("Food")
+                .Returns("Tech");
+
+            // price
+            mockReader.SetupSequence(r => r.GetString(2))
+                .Returns("120")
+                .Returns("13000");
+
+            var repository = new ProductRepository(mockConnection.Object);
+            var service = new ProductService(repository);
+
+            // Act
+            var result = service.GetProductsByCategory(expectedCategory);
+
+            // Assert
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual("Food", result[0].Category);
+            Assert.AreEqual("Pizza", result[0].Name);
+        }
+    }
+}

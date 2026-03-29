47b0667441a1de587aad6d57b9905d1b507f31e8
soheyl228 <soheylizadkhah@gmail.com>
2026-03-29 19:31:50 +0200

    Lab w13 - mocked DB response for ProductManager unit test

--- class/w13/ProductManager.Tests/UnitTest.cs
+++ class/w13/ProductManager.Tests/UnitTest.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using ProductManager;
-using System.Collections.Generic;
 using System.Data;
 
 namespace ProductManager.Tests
@@ -14,41 +13,29 @@ namespace ProductManager.Tests
         public void GetProductsByCategory_UnitTest_WithMock()
         {
             // Arrange
-            var expectedCategory = "Food";
-
             var mockConnection = new Mock<IDbConnection>();
             var mockCommand = new Mock<IDbCommand>();
             var mockReader = new Mock<IDataReader>();
-            var mockParameter = new Mock<IDbDataParameter>();
-            var mockParameters = new Mock<IDataParameterCollection>();
 
-            // Connection
             mockConnection.Setup(c => c.Open());
             mockConnection.Setup(c => c.Close());
             mockConnection.Setup(c => c.CreateCommand()).Returns(mockCommand.Object);
 
-            // Command
             mockCommand.Setup(c => c.ExecuteReader()).Returns(mockReader.Object);
-            mockCommand.Setup(c => c.CreateParameter()).Returns(mockParameter.Object);
-            mockCommand.Setup(c => c.Parameters).Returns(mockParameters.Object);
 
-            // Reader (simulate DB rows)
             mockReader.SetupSequence(r => r.Read())
                 .Returns(true)
                 .Returns(true)
                 .Returns(false);
 
-            // name
             mockReader.SetupSequence(r => r.GetString(0))
                 .Returns("Pizza")
                 .Returns("iPhone 17 Pro");
 
-            // category
             mockReader.SetupSequence(r => r.GetString(1))
                 .Returns("Food")
                 .Returns("Tech");
 
-            // price
             mockReader.SetupSequence(r => r.GetString(2))
                 .Returns("120")
                 .Returns("13000");
@@ -57,12 +44,13 @@ namespace ProductManager.Tests
             var service = new ProductService(repository);
 
             // Act
-            var result = service.GetProductsByCategory(expectedCategory);
+            var result = service.GetProductsByCategory("Food");
 
             // Assert
-            Assert.AreEqual(1, result.Count);
+            Assert.HasCount(1, result);
             Assert.AreEqual("Food", result[0].Category);
             Assert.AreEqual("Pizza", result[0].Name);
+            Assert.AreEqual("120", result[0].Price);
         }
     }
 }
